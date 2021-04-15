5675fb3b2325f79f482e855c2aece42363aedd0b
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2021-04-15 18:57:32 +0200

    passing navigation params between tabs

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -7,11 +7,15 @@ import CompanyDetailsScreen from '../screens/CompanyDetailsScreen';
 
 const { Navigator, Screen } = createBottomTabNavigator<ParamList>();
 
-const TabNavigator = (): React.ReactElement => (
+const TabNavigator = (props): React.ReactElement => {
+  const params = props.route.params;
+  console.log('params', params)
+  return (
   <Navigator>
-    <Screen name={COMPANY_INFO} component={CompanyDetailsScreen} />
-    <Screen name={COMPANY_PERFORMANCE} component={CompanyDetails2Screen} />
+    <Screen name={COMPANY_INFO} component={CompanyDetailsScreen} initialParams={params}/>
+    <Screen name={COMPANY_PERFORMANCE} component={CompanyDetails2Screen} initialParams={params}/>
   </Navigator>
-);
+  )
+};
 
 export { TabNavigator };

e5533ee4dd33e30a9f417e5d1799770c8dde6653
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2021-04-15 09:56:00 +0200

    rename constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const COMPANY_INFO= 'Company info';
+export const COMPANY_INFO= 'Info';
 export const MY_COMPANIES = 'My companies';
-export const COMPANY_PERFORMANCE = 'Company performance';
-export const COMPANY_DETAILS = 'Company_details';
+export const COMPANY_PERFORMANCE = 'Performance';
+export const COMPANY_DETAILS = 'Company details';
